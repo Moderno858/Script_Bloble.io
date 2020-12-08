4d5110fd9a196b0f6d40624bea5553c6c7854aa7
Moderno <71466911+Moderno858@users.noreply.github.com>
2020-12-08 17:28:57 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name          Moderno.Script
+// @namespace    Moderno.Script
+// @version      0.2
+// @description  try to take over the world!
+// @author       Wilker
+// @match        http://bloble.io/*
+// @grant        none
+
