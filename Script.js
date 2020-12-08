7bedeb5290335fd1f267bba60814e2b461d4b3ba
Moderno <71466911+Moderno858@users.noreply.github.com>
2020-12-08 18:21:40 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -69,9 +69,13 @@ level: 0,
     x: 5,
     html: '<div onclick=sell9()>Sell all</div>'
 }, {
-level: 0,
+level: 1,
     x: 5,
     html: '<div onclick=upar()> power plants</div>'
+}, {
+    level: 1,
+    x: 5,
+    html: '<div onclick=upar1()>micro generators</div>'
 }, {
 }
 
@@ -100,3 +104,5 @@ window.sell9 = function (){  for (var a = [], d = 0; d < units.length; ++d)(unit
     }
 window.upar = function (){
  for (var i = 0; i < units.length; ++i) 0 == units[i].type && "hexagon" == units[i].shape && units[i].owner == player.sid && socket.emit("4", units[i].id, 0)}
+window.upar1 = function (){    for (var i = 0; i < units.length; ++i) 3 == units[i].type && "circle" == units[i].shape && units[i].owner == player.sid && socket.emit("4", units[i].id, 1)}
+
